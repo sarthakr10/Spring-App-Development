5744ee37433e56a3643ee04d4d59f28aa03bc8b7
sarthakr10 <rastogisarthak35@gmail.com>
2025-03-01 16:53:41 +0530

    UC2

--- spring/src/main/java/com/spring/spring/HelloController.java
+++ spring/src/main/java/com/spring/spring/HelloController.java
@@ -1,14 +1,28 @@
 package com.spring.spring;
 
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequestMapping("/")
+//public class HelloController {
+//    @GetMapping
+//    public String hello() {
+//        return "Hello from sam ";
+//    }
+//
+//}
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/")
+@Controller
 public class HelloController {
-    @GetMapping
-    public String hello() {
-        return "Hello from sam ";
+    @GetMapping("/hello")
+
+    public String hello(Model model) {
+        model.addAttribute("message", "Hello from Sar");
+        return "hello"; // This will map to hello.html in templates folder
     }
 }
